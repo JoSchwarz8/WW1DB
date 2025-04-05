714d1f5779cc9d840120d9f3582a20cfd70e4b1a
farhaan4567 <168932906+farhaan4567@users.noreply.github.com>
2025-04-05 19:43:38 +0100

    Add files via upload

--- WW1 code/HTML/delete_burial.php
+++ WW1 code/HTML/delete_burial.php
@@ -1,13 +1,15 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ww1_soldiers");
+$conn = new mysqli("localhost", "root", "", "WW1_soldiers");
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+// Get the JSON input
 $data = json_decode(file_get_contents("php://input"), true);
 $surname = $data['surname'];
 
+// SQL to delete the row
 $sql = "DELETE FROM those_buried_in_bradford WHERE surname = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $surname);
